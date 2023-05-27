2c68bf06b3f3b56bb4221881dbe0e4b63c3425d2
mundi <christine.mundi@student.moringaschool.com$ git config --global user.email christine.mundi@student.moringaschool.com>
2023-05-27 09:05:25 +0300

    tests passed

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Playful', true),
+  ('Rowdy', 8, 'M', 'Black', 'Energetic', true),
+  ('Tabitha', 6, 'F', 'Gray', 'Calm', true),
+  ('Sergeant Brown', 10, 'M', 'Brown', 'Brave', true),
+  ('Melissa', 13, 'F', 'Brown', 'Gentle', true),
+  ('Grinch', 3, 'M', 'White', 'Mischievous', true),
+  ('Wendy', 6, 'F', 'Brown', 'Friendly', true),
+  (NULL, NULL, NULL, NULL, NULL, false);
